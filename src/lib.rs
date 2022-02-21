c96af2d10ac390960f8e0bae0dd3ee135d224764
Tsyganok Fedor <ftsiganok@gmail.com>
2022-02-21 20:11:47 +0300

    init

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#![allow(unused_imports, dead_code, unused_variables)]
+struct Solution {}
+
+impl Solution {
+    pub fn placeholder(nums: Vec<i32>) -> i32 {
+        unimplemented!()
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        assert_eq!(Solution::placeholder(vec![7, 1, 5, 3, 6, 4]), 5);
+    }
+}
